aba930b56c54e67023931818848f13abc4b0cde0
Lucia González <luciag.lara@gmail.com>
2021-09-27 02:32:25 +0200

    deploy5

--- server/index.js
+++ server/index.js
@@ -72,7 +72,7 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
   app.use(express.static(path.join(__dirname, "../client/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.join(__dirname, "../client/build/index.html"))
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
   );
 
 
